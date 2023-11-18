bae50b2e99981fe00acad87737d15fd8bd24a08e
Richel Dhaliwal <richeldhaliwal@berkeley.edu>
2023-11-17 17:00:22 -0800

    working on drawing grid

--- src/sawyer/src/draw_grid.py
+++ src/sawyer/src/draw_grid.py
@@ -55,45 +55,85 @@ def main():
             # trans.transform.translation gives current x, y, z
             
             # find curr location of end effector:
-            x = trans.transform.translation.x   # TODO: grid loc x
-            y = trans.transform.translation.y   # TODO: grid loc y
-            z = -0.133 # trans.transform.translation.z   # TODO: grid loc z (marker lifted)
+            x = 0.579 #trans.transform.translation.x   # TODO: grid loc x
+            y = 0.165 #trans.transform.translation.y   # TODO: grid loc y
+            z = -0.145 # trans.transform.translation.z   # TODO: grid loc z (marker lifted)
             
             # trans.transform.translation gives current x, y, z
-            locs = [(x, y, z),(x, y-width, z),(x, y-width, z+height),(x+offs, y-width, z+height),(x+offs, y-width, z),(x+offs, y, z),(x+offs, y, z+height),(x+(2*offs), y-offs, z+height),(x+(2*offs), y-offs, z),(x-offs, y-offs, z),(x-offs, y-offs, z+height),(x-offs, y-(2*offs), z+height), (x-offs, y-(2*offs), z), (x+(2*offs), y-(2*offs), z)]
+            locs = [(x, y, z), (x, y-width/3, z), (x, y-2*width/3, z), (x, y-width, z),(x, y-width, z+height),(x+offs, y-width, z+height), # border horizontal
+                    (x+offs, y-width, z), (x+offs, y-2*width/3, z), (x+offs, y-width/3, z), (x+offs, y, z),(x+offs, y, z+height),(x+(2*offs), y, z+height), #horizontal 1
+                    (x+(2*offs), y, z), (x+(2*offs), y-width/3, z), (x+(2*offs), y-2*width/3, z), (x+(2*offs), y-width, z),(x+(2*offs), y-width, z+height),(x+width, y-width, z+height), #horizontal 2
+                    (x+width, y-width, z), (x+width, y-2*width/3, z), (x+width, y-width/3, z), (x+width, y, z), (x+width, y, z+height) #(x+width, y, z+height),(x+width, y-offs, z+height), # border horizontal
 
-            for x1, y1, z1 in locs:
-                
-                # Set the desired orientation for the end effector HERE (marker touches board)
-                request.ik_request.pose_stamped.pose.position.x = x1
-                request.ik_request.pose_stamped.pose.position.y = y1
-                request.ik_request.pose_stamped.pose.position.z = z1 # TODO: adjust        
 
-                # move robot to loc 1
-                # Send the request to the service
-                response = compute_ik(request)
-                
-                # Print the response HERE
-                print(response)
-                group = MoveGroupCommander("right_arm")
+                    # (x+(2*offs), y, z), (x+(offs), y, z), (x, y, z),(x, y, z+height),(x, y-offs, z+height),  # verical border
+                    # (x, y-offs, z), (x+offs, y-offs, z), (x+ (2 *offs), y-offs, z),(x+width, y-offs, z), (x+width, y-offs, z+height),(x+width, y-(2*offs), z+height), # first vertical
+                    # (x+width, y-(2*offs), z), (x+(2 * offs), y-(2*offs), z), (x + offs, y-(2*offs), z), (x, y-(2*offs), z), (x, y-(2*offs), z+height), (x, y-width, z+height), #second vertical
+                    # (x, y-width, z), (x+offs, y-width, z), (x+ (2 *offs), y-width, z),(x+width, y-width, z) #(x+width, y-offs, z+height),(x+width, y-(2*offs), z+height) # verical border
+                    
+                    ]
+            
+            new_locs = [[x1, y1, z1, 0, 1, 0, 0] for x1, y1, z1 in locs]
+            response = compute_ik(request)
+                    
+            # Print the response HERE
+            print(response)
+            group = MoveGroupCommander("right_arm")
 
-                # Setting position and orientation target
-                group.set_pose_target(request.ik_request.pose_stamped)
+            # Setting position and orientation target
+            group.set_pose_targets(new_locs)
 
-                # Plan IK
-                plan = group.plan()
-                user_input = input("Enter 'y' if the trajectory looks safe on RVIZ")
-                
-                # Execute IK if safe
-                # if user_input == 'y':
-                #     group.execute(plan[1])
+            # Plan IK
+            plan = group.plan()
+            user_input = input("Enter 'y' if the trajectory looks safe on RVIZ")
+            
+            # # Execute IK if safe
+            # # if user_input == 'y':
+            # #     group.execute(plan[1])
 
-                while user_input == 'n':
-                    plan = group.plan()
-                    user_input = input("Enter 'y' if the trajectory looks safe on RVIZ")
-                
-                if user_input == 'y':
-                    group.execute(plan[1])    
+            # while user_input == 'n':
+            #     plan = group.plan()
+            #     user_input = input("Enter 'y' if the trajectory looks safe on RVIZ")
+
+            
+            if user_input == 'y':  
+                group.execute(plan[1])
+            
+            # for i in range(2):
+            #     for x1, y1, z1 in locs:
+                    
+            #         # Set the desired orientation for the end effector HERE (marker touches board)
+            #         request.ik_request.pose_stamped.pose.position.x = x1
+            #         request.ik_request.pose_stamped.pose.position.y = y1
+            #         request.ik_request.pose_stamped.pose.position.z = z1 # TODO: adjust        
+
+            #         # move robot to loc 1
+            #         # Send the request to the service
+            #         response = compute_ik(request)
+                    
+            #         # Print the response HERE
+            #         print(response)
+            #         group = MoveGroupCommander("right_arm")
+
+            #         # Setting position and orientation target
+            #         group.set_pose_target(request.ik_request.pose_stamped)
+
+            #         # Plan IK
+            #         plan = group.plan()
+            #         # user_input = input("Enter 'y' if the trajectory looks safe on RVIZ")
+                    
+            #         # # Execute IK if safe
+            #         # # if user_input == 'y':
+            #         # #     group.execute(plan[1])
+
+            #         # while user_input == 'n':
+            #         #     plan = group.plan()
+            #         #     user_input = input("Enter 'y' if the trajectory looks safe on RVIZ")
+
+                    
+            #         # if user_input == 'y':
+            #         group.execute(plan[1])  
+            # ---------------------------------------------  
 
             # # move robot to loc 2 (draw first line of X)
             # request.ik_request.pose_stamped.pose.position.x += 0.25 # TODO: adjust
