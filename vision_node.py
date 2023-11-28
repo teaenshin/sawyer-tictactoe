2c96fbd317fe4d3464610180ad7eb903ced58940
Pranit Panda <pranitpanda2.0@gmail.com>
2023-11-28 15:00:38 -0800

    update to vision node to loop until correct whiteboard is found and also properly publish board data

--- vision_node.py
+++ vision_node.py
@@ -1,24 +1,37 @@
 import rospy
 import BOARD_DATA 
+from vision.vision import *
 
 class VisionNode:
     def __init__(self):
         rospy.init_node('vision_node')
         self.publisher = rospy.Publisher('board_data_topic', BOARD_DATA, queue_size=10)
         self.rate = rospy.Rate(1)  # 1 Hz
-        #TODO init the center of the grid and the dimensions for cropping
+        self.whiteboard = None
+        while True:
+            color_image = get_color_image()
+            self.whiteboard = get_whiteboard(color_image)
+            cropped_image = crop_image(color_image, self.whiteboard)
+            cv2.imshow("Cropped", cropped_image)
+            user_input = input("Press Y and Enter if Good, any other key if not")
+            if user_input == "Y":
+                break
+        
 
     def publish_board_data(self):
         while not rospy.is_shutdown():
             board_data = BOARD_DATA()
-            board_data.data = self.do_vision()
+            board_data.data = self.get_board()
             rospy.loginfo("Publishing board data: %s", board_data.data)
             self.publisher.publish(board_data)
             self.rate.sleep()
 
-    def do_vision(self):
-        #TODO implement this function using vision.py
-        return [""]*9
+
+    def get_board(self):
+        color_image = get_color_image()
+        cropped_image = crop_image(color_image, self.whiteboard)
+        return getBoard(cropped_image)
+    
 
 if __name__ == '__main__':
     node = VisionNode()
